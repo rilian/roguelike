141cdf839fdd37bd0f4ddb897fa3cd96a0ee733c
rilian <dmitriyis@gmail.com>
2015-09-19 22:55:23 +0300

    map with initial point

--- map.rb
+++ map.rb
@@ -1,34 +1,43 @@
 class Map
-  attr_accessor :raw
+  attr_accessor :map
 
   TILES = {
-    rock: '#'
+    rock: '#',
+    ground: ' '
   }
 
   def initialize(opts={})
     @opts = opts
 
-    @raw = []
+    @map = []
+    @random = Random.new
   end
 
   def generate
     fill_with_rock
+    create_first_room
   end
 
   def draw
-    @opts[:height].times do |w|
-      puts @raw[w].map { |c| TILES[c] }.join('')
+    @opts[:height].times do |row|
+      puts @map[row].map { |c| TILES[c] }.join('')
     end
   end
 
 private
 
+  def create_first_room
+    center_x = @random.rand(@opts[:width])
+    center_y = @random.rand(@opts[:height])
+    @map[center_y][center_x] = :ground
+  end
+
   def fill_with_rock
     @opts[:height].times do |row|
-      @raw << []
-      @opts[:width].times do |_|
-        @raw[row] = [] if @raw[row].nil?
-        @raw[row] << :rock
+      @map << []
+      @opts[:width].times do |_col|
+        @map[row] = [] if @map[row].nil?
+        @map[row] << :rock
       end
     end
   end
