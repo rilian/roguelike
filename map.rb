dbd9ee713f461ea90eb986bee7a195fac0155503
rilian <dmitriyis@gmail.com>
2015-09-20 21:17:31 +0300

    use integer distance

--- map.rb
+++ map.rb
@@ -361,7 +361,8 @@ private
   # end
 
   def distance(p1, p2)
-    Math.sqrt((p1[:x] - p2[:x])**2 + (p1[:y] - p2[:y])**2)
+    #Math.sqrt((p1[:x] - p2[:x])**2 + (p1[:y] - p2[:y])**2)
+    [(p1[:x] - p2[:x]).abs, (p1[:y] - p2[:y]).abs].min
   end
 
   def generate_rooms
