f9e29c17cd8bf3c20bbbd2ffdc557224e192b29f
rilian <dmitriyis@gmail.com>
2015-09-20 18:01:20 +0300

    cleanup code

--- map.rb
+++ map.rb
@@ -31,13 +31,12 @@ class Map
 
   def generate
     @data[:rooms] = generate_rooms
-    @data[:rooms_unconnected] = @data[:rooms].dup
-    @data[:rooms_connected] = []
-    @data[:player] = put_player(*@data[:rooms].first)
+    #@data[:rooms_unconnected] = @data[:rooms].dup
+    #@data[:rooms_connected] = []
+    @data[:player] = put_player(@data[:rooms].first)
     @data[:rooms_density] = rooms_density
-    generate_doors
     #connect_all_rooms
-    generate_path_lines
+    #generate_path_lines
     # cleanup_path_line_dead_ends
     #replace_path_to_ground
   end
@@ -83,32 +82,26 @@ private
     end
   end
 
-  def generate_doors
-    @data[:rooms].each do |room|
-      doors = 0
-      [
-        [room[0] + @rnd.rand(0...room[2]), room[1] - 1],
-        [room[0] - 1, room[1] + @rnd.rand(0...room[3])],
-        [room[0] + @rnd.rand(0...room[2]), room[1] + room[3]],
-        [room[0] + room[2], room[1] + @rnd.rand(0...room[3])],
-      ].shuffle.each do |point|
-        if doors < 3
-          unless [0, 1, @opts[:map_width] - 1, @opts[:map_width] - 2].include?(point.first) || [0, 1, @opts[:map_height] - 1, @opts[:map_height] - 2].include?(point.last)
-            @map[point.last][point.first] = :door
-            if point.first == room[0] - 1
-              @map[point.last][point.first - 1] = :path_start
-            end
-            if point.first == room[0] + room[2]
-              @map[point.last][point.first + 1] = :path_start
-            end
-            if point.last == room[1] -1
-              @map[point.last - 1][point.first] = :path_start
-            end
-            if point.last == room[1] + room[3]
-              @map[point.last + 1][point.first] = :path_start
-            end
-            doors += 1
-          end
+  def generate_doors(room)
+    [
+      [room[:x] + @rnd.rand(0...room[:w]), room[:y] - 1],
+      [room[:x] - 1, room[:y] + @rnd.rand(0...room[:h])],
+      [room[:x] + @rnd.rand(0...room[:w]), room[:y] + room[:h]],
+      [room[:x] + room[:w], room[:y] + @rnd.rand(0...room[:h])],
+    ].shuffle.each do |point|
+      unless [0, 1, @opts[:map_width] - 1, @opts[:map_width] - 2].include?(point.first) || [0, 1, @opts[:map_height] - 1, @opts[:map_height] - 2].include?(point.last)
+        @map[point.last][point.first] = :door
+        if point.first == room[:x] - 1
+          @map[point.last][point.first - 1] = :path_start
+        end
+        if point.first == room[:x] + room[:w]
+          @map[point.last][point.first + 1] = :path_start
+        end
+        if point.last == room[:y] -1
+          @map[point.last - 1][point.first] = :path_start
+        end
+        if point.last == room[:y] + room[:h]
+          @map[point.last + 1][point.first] = :path_start
         end
       end
     end
@@ -183,45 +176,45 @@ private
     end
   end
 
-  def connect_all_rooms
-    return if @data[:rooms_unconnected].empty?
-
-    @data[:rooms_connected] << @data[:rooms_unconnected].pop
-
-    while !@data[:rooms_unconnected].empty?
-      room_1 = @data[:rooms_unconnected].pop
-      center = get_room_center(*room_1)
-      room_2 = @data[:rooms_connected].sort { |a, b|
-        distance(get_room_center(*a), center) <=> distance(get_room_center(*b), center)
-      }.first.dup
-      # @map[center.last][center.first] = :marker
-      # @map[get_room_center(*room_2).last][get_room_center(*room_2).first] = :marker
-
-      connect_rooms(room_1, room_2)
-      @data[:rooms_connected] << room_1
-    end
-  end
-
-  def connect_rooms(room_1, room_2)
-    # puts "\nconnecting rooms\n"
-    # puts "room_1 = #{room_1.inspect}"
-    # puts "room_2 = #{room_2.inspect}"
-    center_1 = get_room_center(*room_1)
-    center_2 = get_room_center(*room_2)
-    # puts "center_1 = #{center_1.inspect}"
-    # puts "center_2 = #{center_2.inspect}"
-
-    diff_x = center_2.first - center_1.first
-    diff_y = center_2.last - center_1.last
-  end
-
-  def get_room_center(x, y, w, h)
-    [x + (w / 2.0).ceil - 1, y + (h / 2.0).ceil - 1]
-  end
-
-  def distance(p1, p2)
-    Math.sqrt((p1.last - p1.first)**2 + (p2.last - p2.first)**2)
-  end
+  # def connect_all_rooms
+  #   return if @data[:rooms_unconnected].empty?
+  #
+  #   @data[:rooms_connected] << @data[:rooms_unconnected].pop
+  #
+  #   while !@data[:rooms_unconnected].empty?
+  #     room_1 = @data[:rooms_unconnected].pop
+  #     center = get_room_center(*room_1)
+  #     room_2 = @data[:rooms_connected].sort { |a, b|
+  #       distance(get_room_center(*a), center) <=> distance(get_room_center(*b), center)
+  #     }.first.dup
+  #     # @map[center.last][center.first] = :marker
+  #     # @map[get_room_center(*room_2).last][get_room_center(*room_2).first] = :marker
+  #
+  #     connect_rooms(room_1, room_2)
+  #     @data[:rooms_connected] << room_1
+  #   end
+  # end
+  #
+  # def connect_rooms(room_1, room_2)
+  #   # puts "\nconnecting rooms\n"
+  #   # puts "room_1 = #{room_1.inspect}"
+  #   # puts "room_2 = #{room_2.inspect}"
+  #   center_1 = get_room_center(*room_1)
+  #   center_2 = get_room_center(*room_2)
+  #   # puts "center_1 = #{center_1.inspect}"
+  #   # puts "center_2 = #{center_2.inspect}"
+  #
+  #   diff_x = center_2.first - center_1.first
+  #   diff_y = center_2.last - center_1.last
+  # end
+  #
+  # def get_room_center(x, y, w, h)
+  #   [x + (w / 2.0).ceil - 1, y + (h / 2.0).ceil - 1]
+  # end
+  #
+  # def distance(p1, p2)
+  #   Math.sqrt((p1.last - p1.first)**2 + (p2.last - p2.first)**2)
+  # end
 
   def generate_rooms
     fill_map_with_rock
@@ -231,7 +224,8 @@ private
     begin
       attempts -= 1
       if room = generate_room
-        fill_room_with_ground(*room)
+        fill_room_with_ground(room)
+        generate_doors(room)
         rooms << room
       end
     end until attempts <= 0 || rooms_density >= @opts[:max_rooms_density]
@@ -241,17 +235,18 @@ private
   def generate_room_dimensions
     top_left = 1 + @rnd.rand(@opts[:map_width] - @opts[:min_room_dimension]), 1 + @rnd.rand(@opts[:map_height] - @opts[:min_room_dimension])
     dimensions = @rnd.rand(@opts[:min_room_dimension]..@opts[:max_room_width]), @rnd.rand(@opts[:min_room_dimension]..@opts[:max_room_height])
-    [top_left.first, top_left.last, dimensions.first, dimensions.last]
+    { x: top_left.first, y: top_left.last, w: dimensions.first, h: dimensions.last }
   end
 
   def generate_room
+    room = {}
     attempts = @opts[:max_single_room_generation_attempts]
     begin
       attempts -= 1
-      x, y, w, h = generate_room_dimensions
-    end until result = room_fit_map?(x, y, w, h) && test_room_is_rock?(x, y, w, h) || attempts <= 0
+      room.merge! generate_room_dimensions
+    end until result = room_fit_map?(room) && test_room_is_rock?(room) || attempts <= 0
 
-    (result && attempts > 0) ? [x, y, w, h] : nil
+    (result && attempts > 0) ? room : nil
   end
 
   def fill_map_with_rock
@@ -265,26 +260,24 @@ private
     end
   end
 
-  def room_fit_map?(x, y, w, h)
-    x + w < @opts[:map_width] && y + h < @opts[:map_height]
+  def room_fit_map?(room)
+    room[:x] + room[:w] < @opts[:map_width] && room[:y] + room[:h] < @opts[:map_height]
   end
 
-  def test_room_is_rock?(x, y, w, h)
-    only_rock = true
-    ((x - @opts[:min_distance_between_rooms]) ...(x + w + @opts[:min_distance_between_rooms])).each do |col|
-      ((y - @opts[:min_distance_between_rooms])...(y + h + @opts[:min_distance_between_rooms])).each do |row|
+  def test_room_is_rock?(room)
+    ((room[:x] - @opts[:min_distance_between_rooms]) ...(room[:x] + room[:w] + @opts[:min_distance_between_rooms])).each do |col|
+      ((room[:y] - @opts[:min_distance_between_rooms])...(room[:y] + room[:h] + @opts[:min_distance_between_rooms])).each do |row|
         if @map[row] && @map[row][col] && @map[row][col] != :rock
-          only_rock = false
-          break
+          return false
         end
       end
     end
-    only_rock
+    true
   end
 
-  def fill_room_with_ground(x, y, w, h)
-    (x...(x + w)).each do |col|
-      (y...(y + h)).each do |row|
+  def fill_room_with_ground(room)
+    (room[:x]...(room[:x] + room[:w])).each do |col|
+      (room[:y]...(room[:y] + room[:h])).each do |row|
         @map[row][col] = :ground
       end
     end
@@ -300,9 +293,9 @@ private
     ground_tiles.to_f / (@opts[:map_height] * @opts[:map_width])
   end
 
-  def put_player(x, y, w, h)
-    player_coords = @rnd.rand((x + 1)...(x + w - 1)), @rnd.rand((y + 1)...(y + h - 1))
+  def put_player(room)
+    player_coords = @rnd.rand((room[:x] + 1)...(room[:x] + room[:w] - 1)), @rnd.rand((room[:y] + 1)...(room[:y] + room[:h] - 1))
     @map[player_coords.last][player_coords.first] = :player
-    [player_coords.first, player_coords.last]
+    { x: player_coords.first, y: player_coords.last }
   end
 end
